2837d2c6da81e30b46012b63b098bdf112460c8c
Tanya7254 <128213566+Tanya7254@users.noreply.github.com>
2023-03-18 17:50:37 +0530

    Update Assignment5.c

--- Assignment5.c
+++ Assignment5.c
@@ -2,28 +2,39 @@
 //Given the score of 'N' students in the class, write a program to compute the maximum score. Use array to store the marks of the students and assume that the maximum number of students
 // in a class is 50. Assume that -1 is used to indicate a student who is absent for the exam. Assume that a student should secure a minimum of 50 marks to pass the exam.
 
-#include<stdio.h>
-int main()
-{
-  
-scores = []
-
-num_students = int(input("Enter the number of students: "))
-for i in range(num_students):
-    score = int(input("Enter the score for student {}: ".format(i+1)))
-    scores.append(score)
-
-max_score = max(scores)
-
-passed_students = []
-failed_students = []
-
-for score in scores:
-    if score == -1:
-        continue
-    elif score >= 50:
-        passed_students.append(score)
-    else:
-        failed_students.append(score)
-  return 0;
+#include <stdio.h>
+int main() 
+  {
+    int scores[50];
+    int i, num;
+    int max = 0;
+    printf("Enter the number of students: ");
+    scanf("%d",&num);
+    for(i = 0; i < num; i++) 
+  {
+        printf("Enter score for student %d: ", i+1);
+        scanf("%d", &scores[i]);
+        if(scores[i] > max)
+          {
+            max = scores[i];
+          }
+    }
+	printf("\nPassed students:\n");
+    for(i = 0; i < num; i++) 
+	{
+        if(scores[i] >= 50 && scores[i] != -1) 
+          {
+            printf("Student %d: %d\n", i+1, scores[i]);
+          }
+        }
+    printf("\nFailed students:\n");
+    for(i = 0; i < num; i++) 
+	{
+        if(scores[i] < 50 && scores[i] != -1) 
+	  {
+            printf("Student %d: %d\n", i+1, scores[i]);
+          }
+        }
+    printf("\nMaximum score: %d\n", max);
+    return 0;
 }
